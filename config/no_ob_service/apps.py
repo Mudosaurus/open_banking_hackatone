58aac8f4445a4a0a6dedc8e3e48a503e431b8025
Антон А. Остроушко <a.ostroushko@mail.ru>
2024-11-08 03:38:13 +0500

    Add files.

---
+++ config/no_ob_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NoObServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'no_ob_service'
---
+++ config/ob_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ObServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ob_service'
