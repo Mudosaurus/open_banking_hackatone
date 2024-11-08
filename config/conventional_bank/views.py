6c0aa4a6ef6db8958588bfaf247080396919851d
Max <RazeTheShadow@yandex.ru>
2024-11-08 14:45:09 +0500

    test jenkins updates code

--- config/conventional_bank/views.py
+++ config/conventional_bank/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello! It's conventional_bank!")
+    return HttpResponse("Hello! It's conventional_bank!!!!")
