965092b2dde42757dc5221a591dbe5575b0eb905
Max <RazeTheShadow@yandex.ru>
2024-11-08 15:36:04 +0500

    also test

--- config/conventional_bank/views.py
+++ config/conventional_bank/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello! It's test conventional_bank!!!!")
+    return HttpResponse("Hello! It's testing conventional_bank!!!!")
