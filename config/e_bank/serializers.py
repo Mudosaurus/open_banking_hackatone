2996f5c661123b37d8beb57ba7b150808e622153
nmikolaychuk <knopka420@gmail.com>
2024-11-19 21:53:02 +0300

    Revert "test e_bank changes"
    This reverts commit 995fc2b2cbaf9e2cfc1a8b116dedd33b5ebbe3c6.

--- config/e_bank/serializers.py
+++
@@ -1,8 +0,0 @@
-from hob_api.serializers import *
-from . import models
-
-ClientSerializer.Meta.model = models.Client
-ValuteSerializer.Meta.model = models.Valute
-AccountSerializer.Meta.model = models.Account
-LoanSerializer.Meta.model = models.Loan
-SalarySerializer.Meta.model = models.Salary
--- config/e_bank/urls.py
+++ config/e_bank/urls.py
@@ -1,18 +1,6 @@
 from django.urls import path, include
 
-from rest_framework import routers
-
-from . import views
-
-
-def get_router() -> routers.DefaultRouter:
-    router = routers.DefaultRouter()
-    router.register(r'clients', views.ClientViewSet)
-    router.register(r'valutes', views.ValuteViewSet)
-    router.register(r'accounts', views.AccountViewSet)
-    router.register(r'loans', views.LoanViewSet)
-    router.register(r'salaries', views.SalaryViewSet)
-    return router
+from hob_api.services import get_router
 
 urlpatterns = [
     path('', include(get_router().urls)),
