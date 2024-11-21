ca8da99186fd174555ea0103000a14e2a5bbde8b
Антон А. Остроушко <a.ostroushko@mail.ru>
2024-11-21 14:18:08 +0500

    Added /api/ to urls.

--- config/config/urls.py
+++ config/config/urls.py
@@ -5,10 +5,10 @@ from hob_api.services import get_swagger_schema_view
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('e_bank/', include('e_bank.urls')),
-    path('style_bank/', include('style_bank.urls')),
-    path('consents/', include('consent_service.urls')),
-    path('hob_api/', include('hob_api.urls')),
-    path('docs/', get_swagger_schema_view().with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
+    path('api/admin/', admin.site.urls),
+    path('api/e_bank/', include('e_bank.urls')),
+    path('api/style_bank/', include('style_bank.urls')),
+    path('api/consents/', include('consent_service.urls')),
+    path('api/hob_api/', include('hob_api.urls')),
+    path('api/docs/', get_swagger_schema_view().with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
